aa6ca616ee021c092a3092e89adc115cbccb8450
neurodynamic <>
2020-05-11 13:17:11 -0700

    adds static/expose_hooks.js file

---
+++ static/expose_hooks.js
@@ -0,0 +1,15 @@
+if (typeof React !== 'undefined') {
+  const {
+    useState,
+    useEffect,
+    useRef,
+    useContext,
+    useReducer,
+    useCallback,
+    useMemo,
+    useRef,
+    useImperativeHandle,
+    useLayoutEffect,
+    useDebugValue
+  } = React;
+}
